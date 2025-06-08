935a5ba8acc8b70b930416026e3f1407716e7a98
lukacs-leon <160414449+lukacs-leon@users.noreply.github.com>
2025-06-08 14:10:56 +0000

    health-diary v0.3

--- Health-diary.rs
+++ Health-diary.rs
@@ -2,17 +2,17 @@ use std ::io;
 fn main () {
       println! ("This application is make some statistics with you r pulse.");
       let mut pulse_vec = vec! [];
-      let mut add_more_pulse = True;
+      let mut add_more_pulse = true;
       while add_more_pulse{
           let mut pulse_input = String ::new();
-          println! ("Pleas enter you r first pulse what you want to use .") ;
-          io::std in ()
-          .read_line (&mut pulse_in put )
-          .expect (11 ERROR! Something went wrong! We couldn't read the data. 11 );
+          println!("Pleas enter you r first pulse what you want to use .");
+          io::std in()
+          .read_line(&mut pulse_in put )
+          .expect("ERROR! Something went wrong! We couldn't read the data.");
           let pulse : u8 = pulse_in put
-              .trim ()
-              .pa rse()
-              .expect ("ERROR! You didn't include a number!");
+              .trim()
+              .parse()
+              .expect("ERROR! You didn't include a number!");
               pulse_vec.push(pulse);
       }
 }
