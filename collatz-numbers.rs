3e3779b22e9372f79d37b4907c54dbdefdca0a91
lukacs-leon <160414449+lukacs-leon@users.noreply.github.com>
2025-06-08 10:26:47 +0000

    collatz-numbers v1.5

--- collatz-numbers.rs
+++ collatz-numbers.rs
@@ -2,7 +2,7 @@
 This code is printing the collatz numbers from the START_NUM.
 The collatz method halves the number if it's pairly, and 3x+1 if not pairly
 */
-const START_NUM: i32 = 12;
+const START_NUM: i32 = 6;
 fn main() {
     println!("this code is printing a collatz-number series from {}", START_NUM);
     let mut prev_num = START_NUM;
@@ -10,8 +10,8 @@ fn main() {
     let mut downs = 0;
     let mut ups = 0;
     while actual_num != 1{
-        if START_NUM % 2 == 1 {
-            actual_num = prev_num * 3 +1;
+        if actual_num % 2 == 1 {
+            actual_num = (prev_num * 3) +1;
             ups += 1;
             println!("{} \u{2191}", actual_num);
         } else {
