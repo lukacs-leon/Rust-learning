d6602a207ab0fe1f2bce0ab338f32452789e88ea
lukacs-leon <160414449+lukacs-leon@users.noreply.github.com>
2025-06-09 13:41:18 +0000

    Health-diary2 v0.2 with max returning

--- Health-diary.rs
+++ Health-diary.rs
@@ -1,10 +1,16 @@
-fn max(pulse_vec: vec!){
+fn max(pulse_vec: Vec<i32>) -> i32 {
+    let mut max_pulse: i32 = 0;
     for i in pulse_vec {
-        println!("{}", i)
+        if i > max_pulse {
+            max_pulse = i;
+        }
     }
+    max_pulse
 }
 fn main () {
     println! ("This application is make some statistics with your pulse.");
     let pulse_vec = vec![54, 65, 86, 45, 65, 96, 120, 57, 192, 364, 76, 87];
+    let max_pulse = max(pulse_vec);
+    println!("{}", max_pulse);
 }
 
