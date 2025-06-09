ba86ce6243e0e09f88df44c7264b88b38ccd4059
lukacs-leon <160414449+lukacs-leon@users.noreply.github.com>
2025-06-09 10:30:08 +0200

    Update Health-diary.rs
    Health-diary v0.7 without user input. Hardcodeing the pulse

--- Health-diary.rs
+++ Health-diary.rs
@@ -1,43 +1,5 @@
-use std::io;
 fn main () {
     println! ("This application is make some statistics with you r pulse.");
-    let mut pulse_vec = vec! [];
-    let mut add_more_pulse = true;
-    println!("Pleas enter your first pulse what you want to use.");
-    let mut pulse_input = String ::new();
-    println!("Pleas enter you r first pulse what you want to use .");
-    io::stdin()
-    .read_line(&mut pulse_input )
-    .expect("ERROR! Something went wrong! We couldn't read the data.");
-    let pulse : u8 = pulse_input;
-        .trim()
-        .parse()
-        .expect("ERROR! You didn't include a number!");
-    pulse_vec.push(pulse);
-    while add_more_pulse {
-        println!("Pleas enter your next pulse.")
-        let mut pulse_input = String::new();
-        io::stdin()
-            .read_line(&mut pulse_input )
-            .expect("ERROR! Something went wrong! We couldn't read the data.");
-        let pulse : u8 = pulse_input
-            .trim()
-            .parse()
-            .expect("ERROR! You didn't include a number!");
-        pulse_vec.push(pulse);
-        let mut add_more_pulse_input = String::new();
-        println!("Do you want to add more pulse? (Y/N)");
-        io::stdin()
-            .read_line(&mut name)
-            .expect("ERROR! Something went wrong! We couldn't read the data.");
-        let name = name.trim();
-        if add_more_pulse_input == "N" {
-            add_more_pulse = false;
-        } else if add_more_pulse_input == "Y" {
-            add_more_pulse_input = true;
-        } else {
-            println!("Somthing went wrong!")
-        }
-      }
+    let mut pulse_vec = vec![54, 65, 86, 45, 65, 96, 120, 57, 192, 364, 76, 87]
 }
 
